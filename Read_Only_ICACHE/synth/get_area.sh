f3ecdade11eae47117910d8bc3f684fab96f78b3
Jack <jacket2@illinois.edu>
2025-09-14 20:13:31 -0500

    cleanup

--- Read_Only_ICACHE/synth/get_area.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-set -e
-
-sed -nr 's/Total cell area: +?([0-9]+?)\.[0-9]+?$$/\1/p' reports/area.rpt
--- jacks_cache_ppl/synth/get_slack.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-set -e
-sed -nr 's/ +slack \([A-Z]+\) +(-?[0-9]+?\.[0-9]+?)$$/\1/p' reports/timing.rpt
